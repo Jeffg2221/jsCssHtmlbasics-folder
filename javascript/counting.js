698d4caa72ee3874a1f6ac629b6cc8e93b4a79b5
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2023-01-17 10:42:09 -0500

    count

---
+++ javascript/counting.js
@@ -0,0 +1,17 @@
+// make an array that counts to 1000
+
+
+ function count () {
+
+    let arr = [];
+
+    for(let i = 0; i <= 1000; i++){
+        arr.push(i)
+
+    }
+
+    return arr
+
+
+}
+console.log(count())
